28ace0904b368d232c197273598e3b3be24e6e3a
Steven Luo <steven@steven676.net>
2013-02-23 21:28:00 -0800

    BN firmware version 1.4.2 kernel source

--- drivers/video/omap2/displays/panel-boxer.c
+++ drivers/video/omap2/displays/panel-boxer.c
@@ -5,7 +5,6 @@
  * Author: Tomi Valkeinen <tomi.valkeinen@nokia.com>
  *
  * Copyright (c) 2010 Barnes & Noble
- * David Bolcsfoldi <dbolcsfoldi@intrinsyc.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published by
